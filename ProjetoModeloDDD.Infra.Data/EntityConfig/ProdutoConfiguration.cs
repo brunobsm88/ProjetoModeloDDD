ae6e9f7001d0c3d5d539116225a66aa9dc0504b6
Bruno Moura <brunobsm88@gmail.com>
2020-02-06 22:40:07 -0300

    first commit

---
+++ ProjetoModeloDDD.Infra.Data/EntityConfig/ProdutoConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using ProjetoModeloDDD.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Text;
+
+namespace ProjetoModeloDDD.Infra.Data.EntityConfig
+{
+    public class ProdutoConfiguration : EntityTypeConfiguration<Produto>
+    {
+        public ProdutoConfiguration()
+        {
+            HasKey(p => p.ProdutoId);
+
+
+            Property(c => c.Nome)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            Property(c => c.Valor)
+                .IsRequired();
+
+            HasRequired(p => p.Cliente)
+                  .WithMany()
+                  .HasForeignKey(p => p.ClienteId);
+        }
+    }
+}
